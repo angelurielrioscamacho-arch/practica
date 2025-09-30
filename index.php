03587cc9107fb4650613aed6f3d3cff90600404a
angelurielrioscamacho-arch <angelurielrioscamacho@gmail.com>
2025-09-30 11:34:54 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <form action="">
+            <h1>Crear usuario</h1>
+            <input type="text" name="name" placeholder="Nombre">
+            <input type="text"name = "lastName" placeholder="Apellido">
+            <input type="text" name = "UserName" placeholder="User Name">
+            <input type="text" name = "Password" placeholder="Password">
+            <input type="text"name = "email" placeholder="Email">
+            <input type="submit" value="Agregar Usuario">
+        </form>
+    </div>
+    <div>
+        <h2>Usuarios Registrados</h2>
+        <table>
+            <thead></thead>
+            <tbody></tbody>
+        </table>
+    </div>
+</body>
+</html>
